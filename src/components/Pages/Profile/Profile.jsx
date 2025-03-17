b066c397f156aae5685e39e611c3314aa471303a
Angela Yepes <angelyep76@gmail.com>
2025-03-17 14:39:52 -0500

    profilejsx

--- src/components/Pages/Profile/Profile.jsx
+++ src/components/Pages/Profile/Profile.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { TechItem } from '../../Layouts/Tech-item/Tech-item'
+import { Skills } from '../../Layouts/Skills/Skills.jsx'
+
 
 export const Profile = () => {
   return (
-    <div>
-      <h2>I'm Profile</h2>
-    </div>
+    <>
+      <h1 className="title">PROFILE</h1>
+
+      <div className='tech-grid' >
+
+        <TechItem />
+
+        <TechItem text='Java' srce='../src/assets/images/iconos/java.png'  />
+
+        <TechItem text='Oracle' srce='../src/assets/images/iconos/oracle.png'/>
+
+        <TechItem text='Node' srce='../src/assets/images/iconos/node.png'/>
+
+        <TechItem text='mongo' srce='../src/assets/images/iconos/mongp.png'/>
+
+        <TechItem text='javaScript' srce='../src/assets/images/iconos/javaScript.png'/>
+
+        <TechItem text='figma' srce='../src/assets/images/iconos/figma.png'/>
+
+        <TechItem text='css' srce='../src/assets/images/iconos/css.png'/>
+
+        <TechItem text='html' srce='../src/assets/images/iconos/html.png'/>
+
+      </div>
+
+      <Skills />
+
+    </>
   )
 }
