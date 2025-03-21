c9aea86b55e8f67a3772688c9ad28f5b47d0acb9
Angela Yepes <angelyep76@gmail.com>
2025-03-21 06:26:40 -0500

    export cons experience.jsx

--- src/components/Pages/Experience/Experience.jsx
+++ src/components/Pages/Experience/Experience.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Card_wrapper } from '../../Layouts/Card-wrapper/Card-wrapper'
+import './experience.css'
 
 export const Experience = () => {
   return (
-    <div>
-      <h2>I'm Experienca</h2>
+    <div className='exp'>
+      <Card_wrapper />
+      <Card_wrapper />
+      <Card_wrapper />
+      <Card_wrapper />
+      <Card_wrapper />
+      <Card_wrapper />
     </div>
+    
   )
 }
+
